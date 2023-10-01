f8e8dfeb2b3a0a1581dd5ceb7485f3fedbb3c6a9
Grayson Miller <grayson.miller124@gmail.com>
2023-09-30 18:12:34 -0600

    now can handle arbitrary clients

--- p2p.go
+++ p2p.go
@@ -7,17 +7,36 @@ import (
 	"net"
 	"os"
 	"strings"
+	"sync"
 )
 
-func handleConnection(conn net.Conn) {
+type ClientState struct {
+	Conn net.Conn
+}
+
+type ServerState struct {
+	Conn    net.Conn
+	Address string
+}
+
+var clients = make(map[string]*ClientState)
+var servers = make(map[string]*ServerState)
+var lock = sync.Mutex{}
+
+func handleClientConnection(conn net.Conn) {
+	defer conn.Close()
+	addr := conn.RemoteAddr().String()
+	clients[addr] = &ClientState{Conn: conn}
+	defer delete(clients, addr)
+
 	reader := bufio.NewReader(conn)
 	for {
 		message, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println("Connection closed.")
+			fmt.Println("Client", addr, "disconnected.")
 			return
 		}
-		fmt.Println("Received:", strings.TrimSpace(message))
+		fmt.Println("Received from", addr, ":", strings.TrimSpace(message))
 	}
 }
 
@@ -27,6 +46,8 @@ func startServer(port string) {
 		fmt.Println(err)
 		return
 	}
+	defer ln.Close()
+
 	fmt.Println("Listening on port:", port)
 	for {
 		conn, err := ln.Accept()
@@ -34,59 +55,78 @@ func startServer(port string) {
 			fmt.Println(err)
 			continue
 		}
-		fmt.Println("Connected to:", conn.RemoteAddr())
-		go handleConnection(conn)
-	}
-}
-
-func startClient(ip, port string) {
-	conn, err := net.Dial("tcp", ip+":"+port)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer conn.Close()
-	fmt.Println("Connected to:", ip+":"+port)
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Send message (or '!exit' to disconnect): ")
-		scanner.Scan()
-		text := scanner.Text()
-		if text == "!exit" {
-			break
-		}
-		conn.Write([]byte(text + "\n"))
+		fmt.Println("Client connected:", conn.RemoteAddr())
+		go handleClientConnection(conn)
 	}
 }
 
 func main() {
 	portPtr := flag.String("p", "8080", "port to listen on")
 	flag.Parse()
-
 	go startServer(*portPtr)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		fmt.Print("Command (or '!exit' to quit): ")
+		fmt.Print("Command: ")
 		scanner.Scan()
 		cmd := scanner.Text()
 
-		if strings.HasPrefix(cmd, "!connect ") {
+		switch {
+		case strings.HasPrefix(cmd, "!connect "):
 			parts := strings.Split(cmd, " ")
 			if len(parts) != 2 {
-				fmt.Println("Invalid command format. Use '!connect host:port'")
+				fmt.Println("Invalid format. Use '!connect host:port'")
 				continue
 			}
-			addressParts := strings.Split(parts[1], ":")
-			if len(addressParts) != 2 {
-				fmt.Println("Invalid address format. Use 'host:port'")
+			address := parts[1]
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				fmt.Println("Error connecting to server:", err)
 				continue
 			}
-			startClient(addressParts[0], addressParts[1])
-		} else if cmd == "!exit" {
-			break
-		} else {
+			servers[address] = &ServerState{Conn: conn, Address: address}
+			fmt.Println("Connected to server:", address)
+
+		case strings.HasPrefix(cmd, "!send "):
+			parts := strings.SplitN(cmd, " ", 3)
+			if len(parts) < 3 {
+				fmt.Println("Invalid format. Use '!send <server> <message>'")
+				continue
+			}
+			serverAddr, message := parts[1], parts[2]
+			server, ok := servers[serverAddr]
+			if !ok {
+				fmt.Println("No server found with the address", serverAddr)
+				continue
+			}
+			server.Conn.Write([]byte(message + "\n"))
+
+		case strings.HasPrefix(cmd, "!sendall "):
+			message := strings.TrimPrefix(cmd, "!sendall ")
+			for _, server := range servers {
+				server.Conn.Write([]byte(message + "\n"))
+			}
+
+		case cmd == "!servers":
+			for addr := range servers {
+				fmt.Println(addr)
+			}
+
+		case cmd == "!clients":
+			for addr := range clients {
+				fmt.Println(addr)
+			}
+
+		case cmd == "!exit":
+			for _, server := range servers {
+				server.Conn.Close()
+			}
+			for _, client := range clients {
+				client.Conn.Close()
+			}
+			os.Exit(0)
+
+		default:
 			fmt.Println("Unknown command.")
 		}
 	}
